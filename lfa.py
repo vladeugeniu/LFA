ae0f918912d09cd069c20a04c189ed9ba31cab5d
Stefan Vlad <h5_em_1@yahoo.com>
2016-04-04 20:16:40 +0300

    update parsing

--- lfa.py
+++ lfa.py
@@ -1,5 +1,19 @@
-lafn = {}
 
+
+
+def check(transition_list, word):
+    """
+    This is a recursive function that check
+    the transition for every symbol in word
+    """
+    
+    for transition in transition_list:
+        
+
+
+
+
+lafn = {}
 f = open("date.in", "r")
 date_in = f.readlines()
 
@@ -7,13 +21,11 @@ symbols = date_in[0]
 states = date_in[1]
 final_states = date_in[2]
 
-for state in states:
-    lafn[state] = []
-
 i = 0
 for line in date_in[3:]:
     lafn[states[i]] = line.split()
+    lafn[states[i]] = [x.strip for x in lafn[states[i]]
     i += 1
-    
+
     
 
